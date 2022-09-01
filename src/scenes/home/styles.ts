ae282a8399c881f2e3b4959b2d3643ff5320a49b
Amish <amir.shabani68@gmail.com>
2022-09-01 16:12:00 +0300

    update home screen

--- src/scenes/home/styles.ts
+++ src/scenes/home/styles.ts
@@ -1,11 +1,22 @@
 import {StyleSheet} from 'react-native';
-import {Colors, Spacing, FontSize} from '../../styles/';
+import {Spacing, Rounded} from '../../styles/';
+import colors from '../../styles/colors';
 
 export const Style = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
-    backgroundColor: Colors.WHITE,
+    width: '100%',
+    height: '100%',
+  },
+  flatlist: {
+    flex: 1,
+    backgroundColor: colors.WHITE,
+    borderTopLeftRadius: Rounded.x6,
+    width: '100%',
+    paddingTop: 20,
+    marginTop: 20,
+    paddingLeft: Spacing.x2,
   },
 });
