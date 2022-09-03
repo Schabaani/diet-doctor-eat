a748265a05c70f144d5a7b79a7c818e916976d26
Amish <amir.shabani68@gmail.com>
2022-09-03 13:24:23 +0300

    add simple transform scale animation to button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,21 +1,44 @@
 import React from 'react';
-import {TouchableOpacity, ViewStyle} from 'react-native';
+import {TouchableOpacity, ViewStyle, Animated, StyleProp} from 'react-native';
 
 type TProps = {
-  style?: ViewStyle;
+  style?: StyleProp<ViewStyle>;
   onPress?: () => void;
   testID?: string;
   children: JSX.Element | JSX.Element[];
 };
 
-export const Button = (props: TProps) => (
-  <TouchableOpacity
-    activeOpacity={0.8}
-    style={props.style}
-    testID={props.testID ?? 'custom-button'}
-    onPress={() => {
-      if (props.onPress) props.onPress();
-    }}>
-    {props.children}
-  </TouchableOpacity>
-);
+export const Button = (props: TProps) => {
+  const animation = new Animated.Value(0);
+  const inputRange = [0, 1];
+  const outputRange = [1, 0.8];
+  const scale = animation.interpolate({inputRange, outputRange});
+
+  const onPressIn = () => {
+    Animated.spring(animation, {
+      toValue: 0.5,
+      useNativeDriver: true,
+    }).start();
+  };
+  const onPressOut = () => {
+    Animated.spring(animation, {
+      toValue: 0,
+      useNativeDriver: true,
+    }).start();
+  };
+  return (
+    <Animated.View style={[{transform: [{scale}]}]}>
+      <TouchableOpacity
+        activeOpacity={0.8}
+        style={props.style}
+        testID={props.testID ?? 'custom-button'}
+        onPressIn={onPressIn}
+        onPressOut={onPressOut}
+        onPress={() => {
+          if (props.onPress) props.onPress();
+        }}>
+        {props.children}
+      </TouchableOpacity>
+    </Animated.View>
+  );
+};
